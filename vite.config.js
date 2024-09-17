5bb4a31a3b916f21bc7244aaa4d7295d52c24ee1
Siarhei Karnialiuk <sp_by@tut.by>
2024-09-17 15:17:30 +0300

    Configure Ant Design Theme

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+});
